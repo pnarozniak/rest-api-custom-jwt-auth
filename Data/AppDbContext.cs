8681776ef007b0c33e59da66e4cfbbe133f4c9c6
xlegodroit <xxvege@gmail.com>
2021-07-12 16:13:59 +0200

    Added User table ef configuration with migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using rest_api_custom_jwt_auth.EfConfigurations;
+using rest_api_custom_jwt_auth.Models;
 
 namespace rest_api_custom_jwt_auth.Data
 {
     public class AppDbContext : DbContext
     {
+        public virtual DbSet<User> Users { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
 
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.ApplyConfiguration(new UserEfConfiguration());
         }
     }
 }
