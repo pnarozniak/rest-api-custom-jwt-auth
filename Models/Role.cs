9f9e3fd9c111bb6679221f4150417329f5bd1021
xlegodroit <xxvege@gmail.com>
2021-07-12 16:29:33 +0200

    Added UserRole table model

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,8 +1,17 @@
-﻿namespace rest_api_custom_jwt_auth.Models
+﻿using System.Collections.Generic;
+
+namespace rest_api_custom_jwt_auth.Models
 {
     public class Role
     {
+        public Role()
+        {
+            UserRoles = new HashSet<UserRole>();
+        }
+
         public int IdRole { get; set; }
         public string Name { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace rest_api_custom_jwt_auth.Models
 {
     public class User
     {
+        public User()
+        {
+            UserRoles = new HashSet<UserRole>();
+        }
+
         public int IdUser { get; set; }
         public string HashedPassword { get; set; }
         public string Email { get; set; }
         public string RefreshToken { get; set; }
         public DateTime? RefreshTokenExpirationDate { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
     }
 }
