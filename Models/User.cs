10370caad80bf5571a906201e6baf4c8a156d426
xlegodroit <xxvege@gmail.com>
2021-07-12 02:05:17 +0200

    Added User table model

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace rest_api_custom_jwt_auth.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string HashedPassword { get; set; }
+        public string Email { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpirationDate { get; set; }
+    }
+}
